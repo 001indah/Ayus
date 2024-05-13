a24496278c437877e786ca24fa353f8591573e5a
001indah <indahbudiati001@gmail.com>
2024-05-13 13:52:35 +0700

    remove unused codes

--- app/Contact/formValidation.tsx
+++ app/Contact/formValidation.tsx
@@ -1,10 +1,10 @@
-import * as Yup from 'yup'
+// import * as Yup from 'yup'
 
-const FormValidation = Yup.object({
-    name: Yup.string().min(8).required("Please Enter name"),
-    email: Yup.string().email("Please Enter Valid Email"),
-    subject: Yup.string().min(5).required("Please Enter Subject"),
-    message: Yup.string().min(10).required("please Enter Subject")
-})
+// const FormValidation = Yup.object({
+//     name: Yup.string().min(8).required("Please Enter name"),
+//     email: Yup.string().email("Please Enter Valid Email"),
+//     subject: Yup.string().min(5).required("Please Enter Subject"),
+//     message: Yup.string().min(10).required("please Enter Subject")
+// })
 
-export default FormValidation
+// export default FormValidation
